79dd51ad0bb98583580737e8d8b583460fa76da8
MyPanel <nea64226@gmail.com>
2022-07-15 10:07:35 +0900

    First Commit

---
+++ src/main/java/com/example/defmarket/DefMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.defmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/defmarket/DefMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.defmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
